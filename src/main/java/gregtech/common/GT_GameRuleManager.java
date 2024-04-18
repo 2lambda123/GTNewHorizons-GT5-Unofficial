95e8e759a798ec4b92b68e1a315baf9591e3b16a
Restyled.io <commits@restyled.io>
2024-04-18 10:45:33 +0000

    Restyled by clang-format

--- src/main/java/gregtech/common/GT_GameRuleManager.java
+++ src/main/java/gregtech/common/GT_GameRuleManager.java
@@ -14,63 +14,65 @@ import net.minecraftforge.event.world.WorldEvent;
 
 public class GT_GameRuleManager extends GT_Proxy {
 
-    public GT_GameRuleManager() {
-        MinecraftForge.EVENT_BUS.register(new GT_GameRuleManager());
-    }
+  public GT_GameRuleManager() {
+    MinecraftForge.EVENT_BUS.register(new GT_GameRuleManager());
+  }
 
-    @Override
-    public boolean isServerSide() {
-        return false;
-    }
+  @Override
+  public boolean isServerSide() {
+    return false;
+  }
 
-    @Override
-    public boolean isClientSide() {
-        return false;
-    }
+  @Override
+  public boolean isClientSide() {
+    return false;
+  }
 
-    @Override
-    public boolean isBukkitSide() {
-        return false;
-    }
+  @Override
+  public boolean isBukkitSide() {
+    return false;
+  }
 
-    @Override
-    public EntityPlayer getThePlayer() {
-        return null;
-    }
+  @Override
+  public EntityPlayer getThePlayer() {
+    return null;
+  }
 
-    @Override
-    public int addArmor(String aArmorPrefix) {
-        return 0;
-    }
+  @Override
+  public int addArmor(String aArmorPrefix) {
+    return 0;
+  }
 
-    @Override
-    public void doSonictronSound(ItemStack aStack, World aWorld, double aX, double aY, double aZ) {
+  @Override
+  public void doSonictronSound(ItemStack aStack, World aWorld, double aX,
+                               double aY, double aZ) {}
 
-    }
+  public static class GameRule {
+    public static final String WEATHER_CYCLE = "runakaii:doWeatherCycle";
+  }
 
-    public static class GameRule {
-        public static final String WEATHER_CYCLE = "runakaii:doWeatherCycle";
-    }
+  private static void addRule(GameRules rules, String name,
+                              String defaultValue) {
+    if (!rules.hasRule(name))
+      rules.addGameRule(name, defaultValue);
+  }
 
-    private static void addRule(GameRules rules, String name, String defaultValue) {
-        if (!rules.hasRule(name)) rules.addGameRule(name, defaultValue);
-    }
+  @SubscribeEvent
+  public void onWorldLoad(WorldEvent.Load evt) {
+    final GameRules rules = evt.world.getGameRules();
+    addRule(rules, GameRule.WEATHER_CYCLE, "true");
+  }
 
-    @SubscribeEvent
-    public void onWorldLoad(WorldEvent.Load evt) {
-        final GameRules rules = evt.world.getGameRules();
-        addRule(rules, GameRule.WEATHER_CYCLE, "true");
-    }
+  // orld.getWorldInfo().isRaining()
+  //     worldinfo.setRaining(false);
 
-//orld.getWorldInfo().isRaining()
-//    worldinfo.setRaining(false);
-
-    @SubscribeEvent(priority = EventPriority.LOW, receiveCanceled = true)
-    public void onPlayerDrops(PlayerDropsEvent event) {
-        World world = event.entityPlayer.worldObj;
-        final GameRules gameRules = world.getGameRules();
-        if (gameRules.getGameRuleBooleanValue(GameRule.WEATHER_CYCLE))
-            if (world.isRemote) return;
-        final EntityPlayer player = event.entityPlayer;
-    }
+  @SubscribeEvent(priority = EventPriority.LOW, receiveCanceled = true)
+  public void onPlayerDrops(PlayerDropsEvent event) {
+    World world = event.entityPlayer.worldObj;
+    final GameRules gameRules = world.getGameRules();
+    if (gameRules.getGameRuleBooleanValue(GameRule.WEATHER_CYCLE))
+      if (world.isRemote)
+        return;
+    final EntityPlayer player = event.entityPlayer;
+  }
 }
